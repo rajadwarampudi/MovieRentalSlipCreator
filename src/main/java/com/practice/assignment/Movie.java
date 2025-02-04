952ece38640427a96f169f7bbf25d7365ef77386
Raja Dwarampudi <rajeshreddy6622@gmail.com>
2025-02-04 11:23:06 +0100

    Initial state of the refactering task

---
+++ src/main/java/com/practice/assignment/Movie.java
@@ -0,0 +1,20 @@
+package com.practice.assignment;
+
+public class Movie {
+    private String title;
+    private String code;
+
+    public Movie(String title, String code) {
+
+        this.title = title;
+        this.code = code;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getCode() {
+        return code;
+    }
+}
